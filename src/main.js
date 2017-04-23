cd95f24983300d0b50fe26b0790e96b89522d83d
qodot <waitingforqodot@gmail.com>
2017-04-23 20:03:48 +0900

    Install firebase vuefire

--- src/main.js
+++ src/main.js
@@ -3,13 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import * as firebase from 'firebase'
+import VueFire from 'vuefire'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAlvdvSaAuHxTyAf9rSyQXf22lKUX-LFaw",
+  authDomain: "showerbucket-1a754.firebaseapp.com",
+  databaseURL: "https://showerbucket-1a754.firebaseio.com",
+  projectId: "showerbucket-1a754",
+  storageBucket: "showerbucket-1a754.appspot.com",
+  messagingSenderId: "203690076097"
+};
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false
+Vue.use(VueFire)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  firebase: {
+    googleProvider: function () {
+      return new firebase.auth.GoogleAuthProvider()
+    }
+  }
 })
