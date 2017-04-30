21496bd230356cb1437bc3e9da08e2a1da9916c9
qodot <waitingforqodot@gmail.com>
2017-04-30 20:56:09 +0900

    Add firebase insert and update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <h3>Showerbucket</h3>
-    <input v-model="link" @keyup.enter="addLink">
+    <h3>{{ bucket.name }}</h3>
+    <input v-model="addedLink" @keyup.enter="addLink">
     <ul>
       <li v-for="link in links">{{ link }}</li>
     </ul>
@@ -9,17 +9,31 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   data() {
     return {
-      link: '',
+      bucket: {
+        name: 'My Bucket'
+      },
+      addedLink: '',
       links: ['www.naver.com', 'www.google.com']
     }
   },
   methods: {
     addLink() {
-      this.links.push(this.link)
-      this.link = ''
+      this.links.push(this.addedLink)
+      this.addedLink = ''
+
+      const newBucketKey = firebase.database().ref().child('buckets').push().key
+
+      let updates = {}
+      updates[`/buckets/${newBucketKey}`] = this.bucket
+      updates[`/links/${newBucketKey}`] = this.links
+
+      const aaa = firebase.database().ref().update(updates)
+      console.log(aaa)
     }
   }
 }
