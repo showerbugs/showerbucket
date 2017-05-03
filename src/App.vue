ab1608e46a7818be840e6aa57a27a7e7c2945437
amazingguni <amazingguni@gmail.com>
2017-05-03 17:02:55 +0900

    Add firebase Auth && Add managing buckets

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="app">
+    <nav>
+      <h1>Shower Bucket</h1>
+      <div v-if="user">
+        <span>{{ user.displayName }}({{ user.email }})</span>
+        <button @click="logout">logout</button>
+      </div>
+      <button @click="loginGoogle" v-else>login with google</button>
+    </nav>
+
+    <hr>
+
+    <input v-model="bucket" @keyup.enter="addBucket">
+    <select v-model="selectedBucket" @change="bucketChanged">
+      <option v-for="bucket in buckets" :value="bucket">{{ bucket.name }}</option>
+    </select>
+
+    <hr>
+
     <h3>{{ bucket.name }}</h3>
-    <input v-model="addedLink" @keyup.enter="addLink">
+    <input v-model="link" @keyup.enter="addLink">
     <ul>
-      <li v-for="link in links">{{ link }}</li>
+      <li v-for="link in links">{{ link.link }}</li>
     </ul>
   </div>
 </template>
@@ -11,30 +29,84 @@
 <script>
 import firebase from 'firebase'
 
+var config = {
+  apiKey: "AIzaSyAlvdvSaAuHxTyAf9rSyQXf22lKUX-LFaw",
+  authDomain: "showerbucket-1a754.firebaseapp.com",
+  databaseURL: "https://showerbucket-1a754.firebaseio.com",
+  projectId: "showerbucket-1a754",
+  storageBucket: "showerbucket-1a754.appspot.com",
+  messagingSenderId: "203690076097"
+};
+
+var db = firebase.initializeApp(config).database()
+
 export default {
   data() {
     return {
-      bucket: {
-        name: 'My Bucket'
+      bucket: '',
+      link: '',
+      user: null,
+      selectedBucket: null,
+    }
+  },
+  firebase() {
+    return {
+      links: db.ref(),
+      buckets: {
+        source: db.ref('buckets'),
+        readyCallback(){
+          if(this.buckets.length > 0)
+            this.selectedBucket = this.buckets[0]
+        },
       },
-      addedLink: '',
-      links: ['www.naver.com', 'www.google.com']
     }
   },
   methods: {
+    addBucket() {
+      if(this.bucket === '') return
+      this.$firebaseRefs.buckets.push({
+        name: this.bucket,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+        updatedAt: firebase.database.ServerValue.TIMESTAMP,
+      })
+      this.bucket = ''
+    },
     addLink() {
-      this.links.push(this.addedLink)
-      this.addedLink = ''
-
-      const newBucketKey = firebase.database().ref().child('buckets').push().key
-
-      let updates = {}
-      updates[`/buckets/${newBucketKey}`] = this.bucket
-      updates[`/links/${newBucketKey}`] = this.links
-
-      const aaa = firebase.database().ref().update(updates)
-      console.log(aaa)
+      if(this.link === '') return
+      this.$firebaseRefs.links.push({
+        link: this.link,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+        updatedAt: firebase.database.ServerValue.TIMESTAMP,
+      })
+      this.link = ''
+    },
+    selectBucket(){
+      this.$bindLinks('links', db.ref('buckets/' + this.bucket['.key']))
+    },
+    loginGoogle(){
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        var token = result.credential.accessToken
+        this.user = result.user
+        console.log(result)
+      }).catch(function(error)  {
+        console.log(error)
+      });
+    },
+    logout(){
+      firebase.auth().signOut().then(() => {
+        this.user = null
+      }, function(error) {
+      });
+    },
+    bucketChanged(){
+      alert(this.selectedBucket.name + ', ' + this.selectedBucket['.key'])
     }
+  },
+  beforeCreate(){
+    firebase.auth().onAuthStateChanged((user) => {
+      this.user = user
+    });
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,19 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueFire from 'vuefire'
 import router from './router'
 import * as firebase from 'firebase'
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyAlvdvSaAuHxTyAf9rSyQXf22lKUX-LFaw",
-  authDomain: "showerbucket-1a754.firebaseapp.com",
-  databaseURL: "https://showerbucket-1a754.firebaseio.com",
-  projectId: "showerbucket-1a754",
-  storageBucket: "showerbucket-1a754.appspot.com",
-  messagingSenderId: "203690076097"
-};
-firebase.initializeApp(config);
+Vue.use(VueFire)
+
+
 
 Vue.config.productionTip = false
 
