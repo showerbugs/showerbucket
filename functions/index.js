5fb6017ccbf5a5797ff6b24e72a65bcddd183968
최민호/협업시스템개발팀/NE <minho-choi@nhnent.com>
2017-05-14 16:38:49 +0900

    Link cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const fetch = require('node-fetch');
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +11,30 @@ var functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+exports.preview = functions.https.onRequest((req, res) => {
+  const url = req.query.url;
+  const mock = {
+    Title: "Python Slack 봇 개발 및 CI 연동 삽질기",
+    Description: "작년 11월, 구글 번역 서비스가 신경망 학습을 적용해서 개편되었다. (관련기사) 벌써 서너달이 지난 일이지만, 현재까지도 웹에서 아티클이나 문서를 읽을 때 구글 번역 크롬 익스텐션의 도움을 많이 받고 있다.",
+    Images: ["https://qodot.github.io/image/google-translate-test.png"]
+  }
+  cors(req, res, () => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(JSON.stringify(mock));
+  });
+  // Grab the text parameter.
+  // const url = req.query.url;
+  // fetch('http://headers.jsontest.com?url='+ url).then(function(res2) {
+  //   console.log(res2.json());
+  //   res.status(200).send(res2.json());
+  // });
+// what you want with response
+
+  // // Push it into the Realtime Database then send a response
+  // admin.database().ref('/links').push({original: original}).then(snapshot => {
+  //   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  //   res.redirect(303, snapshot.ref);
+  // });
+});
--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,9 +1,40 @@
 <template>
-  <li><a :href="link.link">{{ link.link }}</a></li>
+  <li><a :href="link.link">{{ link.link }}</a>
+    <div>
+      <h1>{{preview.Title}}</h1>
+      <h5>{{preview.Description}}</h5>
+      <img :src="preview.Images[0]" width="400"/>
+    </div>
+  </li>
 </template>
 
 <script>
 export default {
-  props: ['link']
+  props: ['link'],
+  data () {
+    return {
+      preview: {}
+    }
+  },
+  methods: {
+    getPreview: async function (url, opt = {}) {
+      opt.headers = {
+        'Method': 'GET',
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      }
+      opt.mode = 'cors'
+      try {
+        let response = await fetch(`https://us-central1-showerbucket-1a754.cloudfunctions.net/preview?url=${url}`, opt);
+        this.preview = await response.json();
+        console.log(this.preview)
+      } catch (e) {
+        return e.message;
+      }
+    }
+  },
+  created() {
+      this.getPreview(this.link.link);
+  }
 }
 </script>
