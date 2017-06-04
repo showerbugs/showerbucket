c2703b4a37d2a68d52b7c4a04803568cc7626ccc
qodot <waitingforqodot@gmail.com>
2017-06-04 15:18:17 +0900

    Add a hook to every routing for auth

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,36 @@ import Router from 'vue-router'
 import HomeView from '../components/HomeView'
 import BucketsView from '../components/BucketsView'
 import LinksView from '../components/LinksView'
+import { firebase } from '../firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
-    {path: '/', component: HomeView, name: 'home'},
-    {path: '/buckets', component: BucketsView, name: 'buckets'},
-    {path: '/buckets/:bucketId', component: LinksView, name: 'links'}
+    { path: '/', component: HomeView, name: 'home' },
+    { path: '/buckets', component: BucketsView, name: 'buckets', meta: { authRequired: true } },
+    { path: '/buckets/:bucketId', component: LinksView, name: 'links' }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.authRequired)) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        next()
+      }
+      else {
+        alert('Please signin first!')
+        next({ name: 'home' })
+      }
+    })
+
+    next(false)
+  }
+  else {
+    next()
+  }
+})
+
+export default router
