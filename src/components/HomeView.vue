5e924a017c1d6cae41579da035ec0ee813d54861
qodot <waitingforqodot@gmail.com>
2017-07-02 18:28:28 +0900

    Set background image at home

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,20 +1,42 @@
 <template>
-  <div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <div class="home-container">
+    <h4>Showerbucket</h4>
+    <p>웹페이지를 수집하고 공유하세요!</p>
   </div>
 </template>
 
 <script>
 import { firebase } from '../firebase'
 
-export default{
+export default {
+  data () {
+    return {
+    }
+  },
   beforeCreate () {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.$router.push({name: 'buckets'})
+        this.$router.push({ name: 'buckets' })
       }
     })
   }
 }
 </script>
+
+<style>
+main {
+  position: relative;
+}
+
+.home-container::after {
+  content: '';
+  background-image: url('../assets/simpson1.png');
+  background-size: 100% 100%;
+  opacity: 0.5;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  position: absolute;
+}
+</style>
